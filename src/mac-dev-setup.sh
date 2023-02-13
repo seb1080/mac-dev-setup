2e72620b4daf2e3fa4d7036d494938c34df0fb8a
seb1080 <sblaisfernandez@gmail.com>
2023-02-13 16:29:36 -0500

    refactor: mac-dev-setup & README.md

--- src/mac-dev-setup.sh
+++ src/mac-dev-setup.sh
@@ -28,17 +28,10 @@ brew install wget
 }>>$MAC_SETUP_PROFILE
 
 # git
-brew install git                                                                                      # https://formulae.brew.sh/formula/git
-# Adding git aliases (https://github.com/thomaspoignant/gitalias)
-git clone https://github.com/thomaspoignant/gitalias.git $INSTALL_FOLDER/gitalias && echo -e "[include]\n    path = $INSTALL_FOLDER/gitalias/.gitalias\n$(cat ~/.gitconfig)" > ~/.gitconfig
-
-brew install git-secrets                                                                              # git hook to check if you are pushing aws secret (https://github.com/awslabs/git-secrets)
-git secrets --register-aws --global
-git secrets --install ~/.git-templates/git-secrets
-git config --global init.templateDir ~/.git-templates/git-secrets
+brew install git
 
 # ZSH
-brew install zsh zsh-completions                                                                      # Install zsh and zsh completions
+brew install zsh zsh-completions
 sudo chmod -R 755 /usr/local/share/zsh
 sudo chown -R root:staff /usr/local/share/zsh
 {
@@ -49,9 +42,11 @@ sudo chown -R root:staff /usr/local/share/zsh
   echo "fi"
 } >>$MAC_SETUP_PROFILE
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"# Install oh-my-zsh on top of zsh to getting additional functionality
-# Terminal replacement https://www.iterm2.com
-brew install --cask iterm2
+
+# Install oh-my-zsh on top of zsh to getting additional functionality
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
 # Pimp command line
 brew install micro                                                                                    # replacement for nano/vi
 brew install lsd                                                                                      # replacement for ls
@@ -78,34 +73,19 @@ echo '. /usr/local/etc/profile.d/z.sh' >> $MAC_SETUP_PROFILE
 
 brew install ctop
 
-# fonts (https://github.com/tonsky/FiraCode/wiki/Intellij-products-instructions)
-brew tap homebrew/cask-fonts
-brew install --cask font-jetbrains-mono
-
 # Browser
 brew install --cask google-chrome
-brew install --cask firefox
+brew install --cask brave
 brew install --cask microsoft-edge
 
 # Music / Video
 brew install --cask spotify
 brew install --cask vlc
 
-# Productivity
-brew install --cask evernote                                                                            # cloud note
-brew install --cask kap                                                                                 # video screenshot
-brew install --cask rectangle                                                                           # manage windows
-
-# Communication
-brew install --cask slack
-brew install --cask whatsapp
-
 # Dev tools
-brew install --cask ngrok                                                                               # tunnel localhost over internet.
 brew install --cask postman                                                                             # Postman makes sending API requests simple.
 
 # IDE
-brew install --cask jetbrains-toolbox
 brew install --cask visual-studio-code
 
 # Language
@@ -113,24 +93,6 @@ brew install --cask visual-studio-code
 mkdir ~/.nvm
 brew install nvm                                                                                     # choose your version of npm
 nvm install node                                                                                     # "node" is an alias for the latest version
-brew install yarn                                                                                    # Dependencies management for node
-
-
-## Java
-curl -s "https://get.sdkman.io" | bash                                                               # sdkman is a tool to manage multiple version of java
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk install java
-brew install maven
-brew install gradle
-
-## golang
-{
-  echo "# Go development"
-  echo "export GOPATH=\"\${HOME}/.go\""
-  echo "export GOROOT=\"\$(brew --prefix golang)/libexec\""
-  echo "export PATH=\"\$PATH:\${GOPATH}/bin:\${GOROOT}/bin\""
-}>>$MAC_SETUP_PROFILE
-brew install go
 
 ## python
 echo "export PATH=\"/usr/local/opt/python/libexec/bin:\$PATH\"" >> $MAC_SETUP_PROFILE
@@ -142,20 +104,13 @@ brew install pyenv
 # shellcheck disable=SC2016
 echo 'eval "$(pyenv init -)"' >> $MAC_SETUP_PROFILE
 
-
-## terraform
-brew install terraform
-terraform -v
-
 # Databases
-brew install --cask dbeaver-community # db viewer
+brew install --cask dbeaver-community
 brew install libpq                  # postgre command line
 brew link --force libpq
+
 # shellcheck disable=SC2016
 echo 'export PATH="/usr/local/opt/libpq/bin:$PATH"' >> $MAC_SETUP_PROFILE
-
-# SFTP
-brew install --cask cyberduck
 
 # Docker
 brew install --cask docker
@@ -163,15 +118,6 @@ brew install bash-completion
 brew install docker-completion
 brew install docker-compose-completion
 brew install docker-machine-completion
-
-# AWS command line
-brew install awscli # Official command line
-pip3 install saws    # A supercharged AWS command line interface (CLI).
-
-# K8S command line
-brew install kubectx
-brew install asdf
-asdf install kubectl latest
 
 # reload profile files.
 {
